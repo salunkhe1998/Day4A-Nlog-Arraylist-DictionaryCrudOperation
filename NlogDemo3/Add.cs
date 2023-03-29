300c161261579f33dd63c7bda6b5c30ba3133296
Ashwini <salunkheashwini1998@gmail.com>
2023-03-29 17:07:42 +0530

    add file for Nlog Config

---
+++ NlogDemo3/Add.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo3
+{
+    public class Add
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Sum(int first, int Second)
+        {
+            if (first <= 0 || Second <= 0)
+            {
+                logger.Warn("Please privide +ve value");
+                logger.Error("Number should not be below zero");
+                logger.Debug("Debug failed");
+            }
+            else
+            {
+                int sum = first + Second;
+                logger.Debug("Debugging with sum operation successfull");
+                logger.Info("Sum Result: " + sum);
+
+            }
+        }
+    }
+}
+
+        
